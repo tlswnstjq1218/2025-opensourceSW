58af443706b7adb508f1e796881b99415cd82494
tlswnstjq1218 <tlswnstjq1218@naver.com>
2025-03-28 11:00:57 +0900

    소스코드 업로드

--- script.py
+++ script.py
@@ -94,3 +94,6 @@ x= {'K':'V',10:20}
 x= int(input())
 y= int(input())
 print(f'{x}+{y} = {x+y}')
+ ##수# 정x = int(input('첫번째 인자를 입력하세요:'))y=int(input('두번쨰 인자를 입력하세요:'))
+print(f'[x] + [y] = [x + y]:
+print('프로그램을 종료합니다')
